dcddbed648b49990b5f52cb1ee1b715c21bc6e46
Louis Granboulan <Louis.Granboulan@airbus.com>
2020-05-26 00:50:36 +0200

    mapper with instructions having labels

--- tests/test_arch_x64_mapper.py
+++ tests/test_arch_x64_mapper.py
@@ -20,3 +20,23 @@ def test_mapper_000():
   i0(m)
   i1(m)
   assert str(m[rcx]) == 'M64(rip+14)'
+
+def test_mapper_001():
+  # create three instructions
+  # movq    %rax, (%rip)
+  # movl    %eax, (%rip)
+  # movq    -16(%rbp), %rcx
+  i0 = cpu.disassemble(b"\x48\x89\x05\x00\x00\x00\x00")
+  i1 = cpu.disassemble(b"\x89\x05\x00\x00\x00\x00")
+  i2 = cpu.disassemble(b"\x48\x8b\x4d\xf0")
+  # modify the first two instructions to insert a label
+  # movq    %rax, foo(%rip)
+  i0.operands[0].a.disp = expressions.lab('foo', size=64)
+  # movb    %eax, bar(%rip)
+  i1.operands[0].a.disp = expressions.lab('bar', size=64)
+  # evaluate those three instructions
+  m=mapper()
+  i0(m)
+  i1(m)
+  i2(m)
+  assert str(m[rcx]) == 'M64(rbp-16)'
